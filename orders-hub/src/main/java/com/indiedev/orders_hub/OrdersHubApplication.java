10f498f28ebdc3633f303d47ca49673d6f0775e8
Rohit Munde <rohitvmunde@gmail.com>
2026-07-29 23:57:57 +0530

    intial commit

---
+++ orders-hub/src/main/java/com/indiedev/orders_hub/OrdersHubApplication.java
@@ -0,0 +1,13 @@
+package com.indiedev.orders_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersHubApplication.class, args);
+	}
+
+}
---
+++ orders-hub/src/test/java/com/indiedev/orders_hub/OrdersHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiedev.orders_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
